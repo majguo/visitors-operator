03e3785486bc16689f60c08f2c61138a663ff470
Jianguo Ma <jiangma@microsoft.com>
2022-07-23 08:49:36 +0800

    config django rest to return a json response as default render class
    Signed-off-by: Jianguo Ma <jiangma@microsoft.com>

--- visitors-service/visitors/settings.py
+++ visitors-service/visitors/settings.py
@@ -29,7 +29,10 @@ ALLOWED_HOSTS = ['*']
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20
+    'PAGE_SIZE': 20,
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ]
 }
 
 # Application definition
